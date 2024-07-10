bb41d9c80a67972ded9e343c7975723ac1b722a5
Kim Malale <kimalale@hotmail.com>
2024-07-10 17:36:34 +0200

    Remove Axios

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const path = require('path');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+
 
 
 const app = express();
-const port = 3005;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
